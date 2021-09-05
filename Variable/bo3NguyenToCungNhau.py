62a208b6baecc4084d375d5b86c3a8ed66c959a7
DinhVanGioi2001 <89461091+DinhVanGioi2001@users.noreply.github.com>
2021-09-06 00:40:14 +0700

    ac update

--- Variable/bo3NguyenToCungNhau.py
+++ Variable/bo3NguyenToCungNhau.py
@@ -1,18 +1,10 @@
 import math
-def lcm(n, m):
-    while n*m != 0:
-        if n > m:
-            n %= m
-        else:
-            m %= n
-    return n+m == 1
 
 str = input().split()
 l = int(str[0])
 r = int(str[1])
-for i in range(l, r - 2):
-    for j in range(l+1, r-1):
-        if lcm(i, j):
-            for k in range(l + 2, r):
-                if lcm(j, k) == 1 and lcm(i, k) == 1:
-                    print("(", i, ", ", j, ", ", k, ")", sep="")
+for i in range(l, r - 1):
+    for j in range(i+1, r):
+        for k in range(j + 1, r + 1):
+            if math.gcd(i, j) == 1 and math.gcd(j, k) == 1 and math.gcd(i, k) == 1:
+                print("(", i, ", ", j, ", ", k, ")", sep="")
